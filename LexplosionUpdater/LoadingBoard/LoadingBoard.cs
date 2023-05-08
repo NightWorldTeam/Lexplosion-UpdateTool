445242f7c5ba17aa7e497290acaed50f5fb95823
Sklaip <SvyatoslavShmelkov@yandex.ru>
2023-05-08 20:07:32 +0500

    Добавьте файлы проекта.

---
+++ LexplosionUpdater/LoadingBoard/LoadingBoard.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace NWUpdater
+{
+    [TemplatePart(Name = PART_BACKGROUND_LAYER, Type = typeof(Grid))]
+    //[TemplatePart(Name = PART_CONTENT_BORDER, Type = typeof(Border))]
+    //[TemplatePart(Name = PART_RECTANGLE, Type = typeof(Rectangle))]
+    //[TemplatePart(Name = PART_PLACEHOLDER, Type = typeof(TextBlock))]
+    public class LoadingBoard : ContentControl
+    {
+        private const string PART_BACKGROUND_LAYER = "PART_Backround_Layer";
+        //private const string PART_CONTENT_BORDER = "PART_Content_Border";
+        //private const string PART_RECTANGLE = "PART_Rectangle";
+        //private const string PART_PLACEHOLDER = "PART_Placeholder";
+
+        #region Properties and Events
+
+
+        public static readonly DependencyProperty IsLoadingFinishedProperty
+            = DependencyProperty.Register("IsLoadingFinished", typeof(bool), typeof(LoadingBoard), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty PlaceholderProperty
+            = DependencyProperty.Register("Placeholder", typeof(string), typeof(LoadingBoard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty RectangeColorProperty
+            = DependencyProperty.Register("RectangeColor", typeof(Brush), typeof(LoadingBoard), new PropertyMetadata(Brushes.White));
+
+        public static readonly DependencyProperty BorderColorProperty
+            = DependencyProperty.Register("BorderColor", typeof(Brush), typeof(LoadingBoard), new PropertyMetadata(Brushes.White));
+
+        public static readonly DependencyProperty BackgroundColorProperty
+            = DependencyProperty.Register("BackgroundColor", typeof(Color), typeof(LoadingBoard), new FrameworkPropertyMetadata(Colors.Transparent));
+
+        public static readonly DependencyProperty BackgroundOpacityProperty
+            = DependencyProperty.Register("BackgroundOpacity", typeof(double), typeof(LoadingBoard), new FrameworkPropertyMetadata(1.0));
+
+        public bool IsLoadingFinished
+        {
+            get => (bool)GetValue(IsLoadingFinishedProperty);
+            set => SetValue(IsLoadingFinishedProperty, value);
+        }
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public Brush RectangeColor
+        {
+            get => (Brush)GetValue(RectangeColorProperty);
+            set => SetValue(RectangeColorProperty, value);
+        }
+
+        public Brush BorderColor
+        {
+            get => (Brush)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+        public Color BackgroundColor
+        {
+            get => (Color)GetValue(BackgroundColorProperty);
+            set => SetValue(BackgroundColorProperty, value);
+        }
+
+        public double BackgroundOpacity
+        {
+            get => (double)GetValue(BackgroundOpacityProperty);
+            set => SetValue(BackgroundOpacityProperty, value);
+        }
+
+        #endregion
+
+
+        #region Constructors
+
+
+        static LoadingBoard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LoadingBoard), new FrameworkPropertyMetadata(typeof(LoadingBoard)));
+        }
+
+
+        #endregion Constructors
+
+
+        #region Public & Protected Methods
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+
+        }
+
+
+        #endregion Public & Protected Methods
+    }
+}
---
+++ LexplosionUpdater/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace NWUpdater
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] _args;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MouseDown += delegate { DragMove(); };
+
+            try
+            {
+                _args = Environment.GetCommandLineArgs();
+
+                if (_args.Length > 6) // эта хуйня нужна потому что в лаунчере была ошибка с передачей аргументов
+                {
+                    string[] newArgs = new string[6];
+
+                    string path = "";
+                    int i;
+                    for (i = 1; i < _args.Length; i++)
+                    {
+                        if (!_args[i].StartsWith("https://"))
+                        {
+                            path += _args[i] + " ";
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    newArgs[1] = path.Remove(path.Length - 1, 1);
+                    newArgs[2] = _args[i];
+
+                    string processName = "";
+                    for (int j = i + 1; j < _args.Length - 2; j++)
+                    {
+                        processName += _args[j] + " ";
+                    }
+
+                    newArgs[3] = processName.Remove(processName.Length - 1, 1);
+                    newArgs[4] = _args[_args.Length - 2];
+                    newArgs[5] = _args[_args.Length - 1];
+
+                    _args = newArgs;
+                }
+
+                Left = Int32.Parse(_args[4]);
+                Top = Int32.Parse(_args[5]);
+
+                new Thread(Update).Start();
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex);
+            }
+        }
+
+        private void Update()
+        {
+            try
+            {
+
+                if (int.TryParse(_args[3], out int id)) // новая версия лаунчера передает pid
+                {
+                    Process.GetProcessById(id).Kill();
+                }
+                else
+                {
+                    foreach (var process in Process.GetProcessesByName(_args[3])) //старая передает имя процесса
+                    {
+                        process.Kill();
+                    }
+                }
+
+                WebClient wc = new WebClient();
+                byte[] fileBytes = wc.DownloadData(_args[2]);
+                using (FileStream fstream = new FileStream(_args[1], FileMode.Truncate))
+                {
+                    fstream.Write(fileBytes, 0, fileBytes.Length);
+                    fstream.Close();
+                }
+
+                Process proc = new Process();
+                proc.StartInfo.FileName = _args[1];
+                proc.Start();
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex);
+            }
+        }
+
+        private void ErrorLog(Exception ex)
+        {
+            MessageBox.Show("Не удалось обновить лаунчер!");
+            try
+            {
+                string name = Environment.ExpandEnvironmentVariables("%appdata%") + "/lexplosion-data/updater-crash-report_" + DateTime.Now.ToString("dd.MM.yyyy-h.mm.ss") + ".log";
+                string dirName = Path.GetDirectoryName(name);
+                if (!Directory.Exists(dirName))
+                {
+                    Directory.CreateDirectory(dirName);
+                }
+
+                using (FileStream fstream = new FileStream(name, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(ex.ToString() + "\r\n\r\nArguments: '" + string.Join(" ", _args ?? new string[0]) + "'");
+                    fstream.Write(bytes, 0, bytes.Length);
+                    fstream.Close();
+                }
+            }
+            catch
+            {
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
